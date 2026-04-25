46cc12f414cf1779e48f1386c415aa4708294df3
Dennis Lang <landenlabs2@gmail.com>
2026-04-25 10:46:19 -0400

    cleanup

--- install-llpeak.bat
+++ install-llpeak.bat
@@ -1,14 +1,48 @@
 @echo off
 
-@echo Copy Release to d:\opt\bin
-copy llpeak-ms\x64\Release\llpeak.exe d:\opt\bin\llpeak.exe
+set prog=llpeak
+
+set dstdir=%bindir%
+if not exist "%dstdir%" (
+ if exist c:\opt\bin  set dstdir=c:\opt\bin
+ if exist d:\opt\bin  set dstdir=d:\opt\bin
+)
 
 
-@echo
+if not exist "%msbuild%" (
+echo Fall back msbuild not found at "%msbuild%"
+set msbuild=F:\opt\VisualStudio\2022\Preview\MSBuild\Current\Bin\MSBuild.exe
+)
+
+cd %prog%-ms
+@echo Clean %proj% 
+rmdir /s x64 2> nul
+rmdir /s %prog%\x64
+
+@echo.
+@echo Build release target
+"%msbuild%" %prog%.sln -p:Configuration="Release";Platform=x64 -verbosity:minimal  -detailedSummary:True 
+cd ..
+
+@echo.
+@echo ---- Build done 
+if not exist "%prog%-ms\x64\Release\%prog%.exe" (
+   echo Failed to build %prog%-ms\x64\Release\%prog%.exe
+   goto _end
+)
+
+@echo.
+@echo Copy Release to %dstdir%
+:: dir %prog%-ms\x64\Release\%prog%.exe
+copy %prog%-ms\x64\Release\%prog%.exe %dstdir%\%prog%.exe
+
+@echo.
 @echo Compare md5 hash
-cmp -h llpeak-ms\x64\Release\llpeak.exe d:\opt\bin\llpeak.exe
-ld -a d:\opt\bin\llpeak.exe
+:: cmp -h %prog%-ms\x64\Release\%prog%.exe %dstdir%\%prog%.exe
+ld -a -ph %prog%-ms\x64\Release\%prog%.exe %dstdir%\%prog%.exe
 
-@echo
-@echo List all llpeak.exe
-ld -r -F=llpeak.exe bin d:\opt\bin
+@rem play happy tone
+rundll32.exe cmdext.dll,MessageBeepStub
+@rem rundll32 user32.dll,MessageBeep
+
+:_end
