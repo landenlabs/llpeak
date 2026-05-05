9399a7c10d43e24110fe74350fe2588bef4dc5c8
Dennis Lang <landenlabs2@gmail.com>
2026-05-05 12:54:25 -0700

    new

--- llpeak/llpeak.cpp
+++ llpeak/llpeak.cpp
@@ -13,7 +13,7 @@
 //
 // ----- License ----
 //
-// Copyright (c) 2022 Dennis Lang
+// Copyright (c) 2026 Dennis Lang
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -37,7 +37,7 @@
 #pragma warning( disable : 4291 )
 #endif
 #define _CRT_SECURE_NO_WARNINGS
-#define VERSION "v6.05.01"
+#define VERSION "v6.05.04"
 
 // Project files
 #include "CmdBlendF.hpp"
